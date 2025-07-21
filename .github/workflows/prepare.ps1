f88e57442db0e5bbcd431232b0ec52578e61737d
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-07-22 01:17:08 +0200

    0.1.0

---
+++ .github/workflows/prepare.ps1
@@ -0,0 +1,36 @@
+if ($env:_BUILD_BRANCH -eq "refs/heads/master" -Or $env:_BUILD_BRANCH -eq "refs/tags/canary") {
+  $env:_IS_BUILD_CANARY = "true"
+  $env:_IS_GITHUB_RELEASE = "true"
+}
+elseif ($env:_BUILD_BRANCH -like "refs/tags/*") {
+  $env:_BUILD_VERSION = $env:_BUILD_VERSION.Substring(0, $env:_BUILD_VERSION.LastIndexOf('.')) + ".0"
+  $env:_IS_GITHUB_RELEASE = "true"
+}
+$env:_RELEASE_VERSION = "v${env:_BUILD_VERSION}"
+
+Write-Output "--------------------------------------------------"
+Write-Output "RELEASE VERSION: $env:_RELEASE_VERSION"
+Write-Output "--------------------------------------------------"
+
+Write-Output "_BUILD_VERSION=${env:_BUILD_VERSION}" >> ${env:GITHUB_ENV}
+Write-Output "_RELEASE_VERSION=${env:_RELEASE_VERSION}" >> ${env:GITHUB_ENV}
+Write-Output "_IS_BUILD_CANARY=${env:_IS_BUILD_CANARY}" >> ${env:GITHUB_ENV}
+Write-Output "_IS_GITHUB_RELEASE=${env:_IS_GITHUB_RELEASE}" >> ${env:GITHUB_ENV}
+
+$modXmlPath = ".\src\mod.xml"
+
+# Lint XML file
+Write-Output "Linting $modXmlPath..."
+xmllint --noout $modXmlPath
+
+# Update Mod version
+$modVersion = $env:_BUILD_VERSION
+[regex]$pattern = "\."
+$modVersion = $pattern.replace($modVersion, ",", 1)
+$modVersion = $modVersion.Replace('.', '')
+(Get-Content $modXmlPath).Replace('${MOD_VERSION}', $modVersion) | Set-Content $modXmlPath
+
+# Start the packaging
+mkdir .dist\ | Out-Null
+Copy-Item LICENSE .\src
+iroga pack .\src --output ".\.dist\${env:_RELEASE_NAME}-${env:_RELEASE_VERSION}.iroj"
---
+++ README.md
@@ -0,0 +1,17 @@
+![License](https://img.shields.io/github/license/tsunamods-codes/the-debug-room) ![Overall Downloads](https://img.shields.io/github/downloads/tsunamods-codes/the-debug-room/total?label=Overall%20Downloads) ![Latest Stable Downloads](https://img.shields.io/github/downloads/tsunamods-codes/the-debug-room/latest/total?label=Latest%20Stable%20Downloads&sort=semver) ![Latest Canary Downloads](https://img.shields.io/github/downloads/tsunamods-codes/the-debug-room/canary/total?label=Latest%20Canary%20Downloads) ![GitHub Actions Workflow Status](https://github.com/tsunamods-codes/the-debug-room/actions/workflows/main-0.1.0.yml/badge.svg?branch=main)
+
+# The Debug Room
+
+A FF8 Debug room mod for modders needing to test core engine functionalities.
+
+## Special Thanks
+
+This mod would have never been possible without the contribution of [myst6re](https://github.com/myst6re). See https://forums.qhimm.com/index.php?topic=13322.0 for more information.
+
+# How to use
+
+> **PLEASE NOTE:** You can also import the mod manually by downloading the ZIP from the [releases page](https://github.com/tsunamods-codes/the-debug-room/releases) and import it via 7th Heaven as a folder
+
+- Install the mod from the catalog
+- Make sure the `save` folder is in the `Additional Folders to Watch` list, in the `Settings -> General Settings` page
+- Enable the mod and load the Slot 1 -> Save 1 to enter the debug room
